3d4ea33cea9c81b31ad10582d535e214a435690b
Stephen Manukonnda <stephen.manukonda95@gmail.com>
2021-06-02 23:28:02 +0530

    all commit

--- src/main/java/com/learn/SpringAnnotationsNoXml/College.java
+++ src/main/java/com/learn/SpringAnnotationsNoXml/College.java
@@ -3,6 +3,7 @@ package com.learn.SpringAnnotationsNoXml;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component("collegeBean")
